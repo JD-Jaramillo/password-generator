f29c959aa8e9b99eb1f8ef4e4855aaee7869e079
JD-Jaramillo <jaramillo784@gmail.com>
2021-02-13 17:25:23 -0500

    committing to save changes

--- code.js
+++ code.js
@@ -1,48 +1,59 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 
-
-function myFunction() {
-    console.log('my function ran');
-}
 function generatePassword() {
     console.log("generate password function ran");
     var passwordLength = prompt("How long do you want your password to be? Please choose a number between 8 and 128");
-
+    console.log(passwordLength, "passwordlength is working");
     // console.log('passwordLength.length', passwordLength.length);
 
     // This is an array
     var specialCharacterArray = ["!", "$", "&", "#", "*"];
     var uppercaseLetterArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     var lowercaseLetterArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    let characterOptionsArray = [];
 
+    let uppercaseLetters;
+    let lowercaseLetters;
+    let specialCharacters;
 
     if (passwordLength < 8 || passwordLength > 128) {
         alert('please start over with 8-128');
     } else if (passwordLength >= 8 && passwordLength < 129) {
         console.log("The number entered is valid");
-        var uppercaseLetters = confirm("Great, now do you want to use uppercase letters? ");
-        console.log("UpperCse leter", uppercaseLetters);
-        var lowercaseLetters = confirm("Do you want a lowercaseLetter?");
-        console.log("lowercase letters", lowercaseLetters);
-        var specialCharacters = confirm("Do you want special characters?");
-        console.log("special characters", specialCharacters);
+        let uppercaseLetters = confirm("Great, now do you want to use uppercase letters? ");
+        if (uppercaseLetters) {
+            console.log('upercase letters true');
+            characterOptionsArray = characterOptionsArray.concat(uppercaseLetterArray);
+            console.log('characterOptionsArray array', characterOptionsArray);
+        }
+        // return uppercaseLetters;
+        // console.log("UpperCse leter", uppercaseLetters);
+        // let lowercaseLetters = confirm("Do you want a lowercaseLetter?");
+        // console.log("lowercase letters", lowercaseLetters);
+        // let specialCharacters = confirm("Do you want special characters?");
+        // console.log("special characters", specialCharacters);
+
     }
 
-    // if (passwordLength >= 8 && passwordLength <= 128) {
-    //   console.log("its long enough");
-    //   console.log('PASSWORDLENGTH', passwordLength);
-    //   return passwordLength;
-    // } else {
-    //   var passwordLength = prompt("Icorrect answer, try again");
-    //   console.log('second time around in else', passwordLength);
-    //   console.log("It's not long enugh");
-    //   return passwordLength;
-    // }
+    let password = "";
+
+    if (uppercaseLetters) {
+        console.log("this is working now ");
+    }
+    console.log('meh2');
+
+    for (i = 1; i <= passwordLength; i++) {
+        // generate random number to use as the index for what key to tgrab in characterOptionsArray
+        console.log('characterOptionsArray.length', characterOptionsArray.length);
+        let randomNumberToUseAsIndex = (Math.floor(Math.random() * characterOptionsArray.length) + 1);
+        console.log('THIS IS THE RANDOM NUMBER INDEX', randomNumberToUseAsIndex)
+        password += characterOptionsArray[randomNumberToUseAsIndex];
+        console.log(password, "this is password wiht random number");
+    }
 
 
-    // console.log('passwordLength', passwordLength);
 
 }
 // this function needs to return the password (generatePassword = "string")
